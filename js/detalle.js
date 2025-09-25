10d357271d2b328ae657caeb39e44e752044352b
Juand12348 <156850435+Juand12348@users.noreply.github.com>
2025-09-25 21:02:39 +0000

    detalle.js

--- js/detalle.js
+++ js/detalle.js
@@ -4,7 +4,12 @@ async function Pokemon(parametro){
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${parametro}`);
         const data = await res.json();
 
-        console.log(data.forms[0].name);
+        root.innerHTML = `<div></div> ${data.forms[0].name}`;
 }
 
 Pokemon("ditto");
+Pokemon(45);
+Pokemon(14);
+Pokemon(1);
+Pokemon(8);
+Pokemon(4);
