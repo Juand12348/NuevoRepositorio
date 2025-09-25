809a50f1c5aa4da5de59e21babfb4f58e382f43b
Juand12348 <156850435+Juand12348@users.noreply.github.com>
2025-09-25 21:28:52 +0000

    index.html

--- js/detalle.js
+++ js/detalle.js
@@ -4,7 +4,25 @@ async function Pokemon(parametro){
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${parametro}`);
         const data = await res.json();
 
-        root.innerHTML = `<div></div> ${data.forms[0].name}`;
+
+    let tipoPoke = "";
+    for(let i = 0; i < data.types.length; i++){
+        tipoPoke += `<span>${data.types[i].type.name}</span> `;
+    }
+
+    root.innerHTML = `
+    <section class="c-detalle">
+            <img src="" alt="">
+            <p>${data.name}</p>
+            <p>${data.id}</p>
+            <p>${tipoPoke}</p>
+            <p>${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
+            <p>${data.stats[0].base_stat}</p>
+            <p>${data.stats[5].base_stat}</p>
+            <p>${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
+            <p>${data.stats[3].base_stat} Defensa Especial: </p>
+    </section>
+    `;
 }
 
 Pokemon("ditto");
